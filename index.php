e62191edf8b533e3f93158d7350262cfc50bba17
emmanuelyardamah <74291643+emmanuelyardamah@users.noreply.github.com>
2020-11-24 14:05:58 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intro to Web Design</title>
+</head>
+<body>
+
+    <h1 class="h1">Emmanuel Yardamah</h1>
+
+    <a href="">One</a>,
+    <a href="">Two</a>,
+    <a href="">Three</a>,
+    <a href="">Four</a>
+
+    <h2>Favorite Foods</h2>
+
+    <ul>
+        <li>Fruit</li>
+        <li>Mango</li>
+        <li>Beans</li>
+        <li>Orange</li>
+    </ul>
+
+    <h2>Achievements</h2>    
+   
+    <h4>Progress in this Course (100%) <progress id="file" value="100" max="100"> 100% </progress></h4>
+    <h4>Progress in the Specification Capstone (20%) <progress id="file" value="20" max="100"> 20% </progress></h4>
+    <h4>Progress in Networking (90%) <progress id="file" value="90" max="100"> 90% </progress></h4>
+
+    <h1>More About Me</h1>    
+    <details>
+        <summary>My Childhood</summary>
+        <p>Optimisizism in Networkng allowed me to pursue my dream and given the right opportunity I will achieve.</p>
+    </details>
+
+    <p>
+        <img src="http://www.intro-webdesign.com/images/newlogo.png" alt="">
+                This page was created Emmanuel Yardamah &amp; Colleen van Lent. To learn more about web design, visit 
+                <a href="http://www.intro-webdesign.com">Intro to Web Design.</a>
+    </p>
+</body>
+</html>
